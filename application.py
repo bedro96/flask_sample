384fd84b6f7657103f58ef0451fd6a0dabfd338a
-kunho <kunho.ko@microsoft.com>
2019-06-03 02:04:51 +0900

    do it

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! just for now"
+    return "Hello World! just for now {{% os.getcwd() %}}"
